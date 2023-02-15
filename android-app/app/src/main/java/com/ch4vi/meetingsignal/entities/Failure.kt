ed1958497bfe18d0b6f17f2aa02fd2fe1d4c128c
Xavi Anyo <xavi.anyo@xing.com>
2023-02-15 22:37:21 +0100

    Added utils and permissions

--- android-app/app/src/main/java/com/ch4vi/meetingsignal/entities/Failure.kt
+++ android-app/app/src/main/java/com/ch4vi/meetingsignal/entities/Failure.kt
@@ -1,7 +1,10 @@
 package com.ch4vi.meetingsignal.entities
 
 sealed class Failure(msg: String? = null) : Throwable(msg) {
-    class PermissionFailure(msg: String) : Failure(msg)
+    class PermissionFailure(msg: String? = null) : Failure(msg)
+    class NotSupportedFailure(msg: String? = null) : Failure(msg)
+    class ScanFailure(msg: String? = null) : Failure(msg)
+
     class ConnectionFailure(msg: String) : Failure(msg)
     class UnexpectedFailure(msg: String? = null) : Failure(msg)
 }
--- android-app/app/src/main/java/com/ch4vi/meetingsignal/utils/Event.kt
+++ android-app/app/src/main/java/com/ch4vi/meetingsignal/utils/Event.kt
@@ -2,6 +2,7 @@ package com.ch4vi.meetingsignal.utils
 
 import androidx.lifecycle.Observer
 
+@Deprecated(message = "looks not needed with compose observeAsState()")
 class Event<out T>(private val content: T) {
 
     private var isUsed = false
@@ -17,6 +18,7 @@ class Event<out T>(private val content: T) {
     fun forceGet(): T = content
 }
 
+@Deprecated(message = "looks not needed with compose observeAsState()")
 class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
     override fun onChanged(value: Event<T>) {
         value.get()?.let { v ->
@@ -25,4 +27,4 @@ class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Obser
     }
 }
 
-fun <T> T.toEvent() = Event(this)
+//fun <T> T.toEvent() = Event(this)
