fd170129e750995e39fc612f5c6f9d408aa75267
Xavi Anyo <xavi.anyo@xing.com>
2023-02-22 21:33:42 +0100

    Update Failure.kt

--- android-app/app/src/main/java/com/ch4vi/meetingsignal/entities/Failure.kt
+++ android-app/app/src/main/java/com/ch4vi/meetingsignal/entities/Failure.kt
@@ -2,9 +2,8 @@ package com.ch4vi.meetingsignal.entities
 
 sealed class Failure(msg: String? = null) : Throwable(msg) {
     class PermissionFailure(msg: String? = null) : Failure(msg)
-    class NotSupportedFailure(msg: String? = null) : Failure(msg)
-    class ScanFailure(msg: String? = null) : Failure(msg)
-
     class ConnectionFailure(msg: String) : Failure(msg)
+    class ScanFailure(msg: String? = null) : Failure(msg)
+    class NotSupportedFailure(msg: String? = null) : Failure(msg)
     class UnexpectedFailure(msg: String? = null) : Failure(msg)
 }
